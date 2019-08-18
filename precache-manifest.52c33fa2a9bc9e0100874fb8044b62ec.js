64279f63d4cedb9e268d0e6874d315e3a6001eea
Alessandro Moreira <alessandro.moreira@gmail.com>
2019-08-18 17:50:42 -0300

    Updates

---
+++ precache-manifest.52c33fa2a9bc9e0100874fb8044b62ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d485165f1a641b4d8556ecd4a3688ba1",
+    "url": "/volleyball_rules/index.html"
+  },
+  {
+    "revision": "40c562c2a11fe84236e6",
+    "url": "/volleyball_rules/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "63601854a48381d10843",
+    "url": "/volleyball_rules/static/js/2.e323c109.chunk.js"
+  },
+  {
+    "revision": "40c562c2a11fe84236e6",
+    "url": "/volleyball_rules/static/js/main.fdc554c9.chunk.js"
+  },
+  {
+    "revision": "6da34c9ef4b86dc6ef2d",
+    "url": "/volleyball_rules/static/js/runtime~main.a3943d35.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/volleyball_rules/static/media/logo.5d5d9eef.svg"
+  }
+]);
